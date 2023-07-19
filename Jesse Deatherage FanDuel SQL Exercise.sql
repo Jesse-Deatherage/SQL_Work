22302dd4cbd6a8eb3315c8283ecd34a6b035e00a
Jesse Detherage <127569088+Jesse-Deatherage@users.noreply.github.com>
2023-07-19 15:47:14 -0400

    Update Jesse Deatherage FanDuel SQL Exercise.sql

--- Jesse Deatherage FanDuel SQL Exercise.sql
+++ Jesse Deatherage FanDuel SQL Exercise.sql
@@ -27,7 +27,9 @@ GROUP BY u.playerid, u.alias, u.email;
 --#4. Parlay bets as a percentage of total bet count from January to April 2019
 SELECT 
     DATE_FORMAT(a.betplaceddate, '%Y-%m') as Month,
-    COUNT(IF(a.bettype = 'Parlay', 1, NULL)) / COUNT(a.betid) * 100 as parlay_percentage
+    COUNT(*) as total_bets,
+    SUM(CASE WHEN a.bettype = 'Parlay' THEN 1 ELSE 0 END) as parlay_bets,
+    (SUM(CASE WHEN a.bettype = 'Parlay' THEN 1 ELSE 0 END)/ COUNT(*)) * 100 as percentage_parlay
 FROM activity a
 WHERE a.betplaceddate BETWEEN '2019-01-01' AND '2019-04-30'
 GROUP BY Month;
@@ -36,7 +38,9 @@ GROUP BY Month;
 --#5. Registration to activation ratio from January to April 2019
 SELECT 
     DATE_FORMAT(u.registration_date, '%Y-%m') as Month,
-    COUNT(CASE WHEN u.activation_date IS NOT NULL THEN 1 END) / COUNT(u.playerid) as reg_to_act_ratio
+    COUNT(*) as total_registered,
+    SUM(CASE WHEN u.activation_date IS NOT NULL THEN 1 ELSE 0 END) as total_activated,
+    (SUM(CASE WHEN u.activation_date IS NOT NULL THEN 1 ELSE 0 END) / COUNT(*)) * 100 as reg_to_act_ratio
 FROM user u
 WHERE u.registration_date BETWEEN '2019-01-01' AND '2019-04-30'
 GROUP BY Month;
